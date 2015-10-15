9a43073e0dbd81214f55d6a773ac519e5a4be40b
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2015-10-15 16:25:02 -0400

    Added lxml to requirements. (Needed by BeautifulSoup)

--- app/imgsearch.py
+++ app/imgsearch.py
@@ -1,32 +1,33 @@
 import random
 from string import ascii_uppercase, digits
 import os
+
+
 class ImgSearch(object):
     img_exts = ['jpg', 'jpeg', 'png', 'gif', 'bmp']
+
     def get_filename(self, filename):
         """
         Generate 50-char long random filename
         """
-        s = ''.join(random.choice(ascii_uppercase + digits) for _ in
-                       range(50))
+        s = ''.join(random.choice(ascii_uppercase + digits) for _ in range(50))
 
         name, ext = os.path.splitext(filename.lower())
         ext = ext.strip('.')
         if not ext in self.img_exts:
-            raise TypeError('{} is not a valid image file\
-                            extension'.format(ext))
+            raise TypeError('{} is not a valid image file extension'.format(ext))
 
         return '{name}.{ext}'.format(name=s, ext=ext)
-    
+
     def parse_google_query(self, html):
         """
         Parses the html from the Google reverse image search
 
-        Returns a response object like the following:
+        :param html:
+        :return dict:
             {'query': `Google's best guess for a query`,
              'results': [link1, link2, link3...]
              }
-
         """
         from bs4 import BeautifulSoup
         import json
--- app/views.py
+++ app/views.py
@@ -5,8 +5,7 @@
 from __future__ import print_function
 from app import app, socketio, socklogger, logger, imgsch
 import flask
-from flask import render_template, url_for, g, send_from_directory,\
-request, session
+from flask import render_template, url_for, g, send_from_directory, request, session
 from flask.ext.socketio import send, emit
 from werkzeug import secure_filename
 
@@ -17,9 +16,11 @@ import requests
 from urlparse import urljoin
 from threading import Timer
 
+
 @app.route('/')
 def root():
     return render_template('upload_root.html')
+
 
 @app.route('/upload', methods=['POST'])
 def receive_upload():
@@ -31,7 +32,6 @@ def receive_upload():
     On load, the client results page connects to a websocket, which
     triggers the API calls. Results are streamed back to the client
     as they come in.
-
     """
     files = flask.request.files.getlist('file[]')
     d = {}
@@ -53,20 +53,23 @@ def receive_upload():
     
     return render_template('results_gallery.html')
 
+
 @app.route('/_/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_DIR'], filename)
 
+
 @socketio.on('connect', namespace='/gallery')
 def stream_results():
     print('Connected to /gallery')
-    
 
     my_url = request.host_url
 
     # request results from Google
-    headers = {}
-    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    headers = {
+        'User-Agent':
+            "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    }
 
     # loop through objects in flask global object
     for filename, actual_name in session['file_dict'].iteritems():
@@ -87,12 +90,12 @@ def stream_results():
             emit('result', json.dumps(result))
     
         else:
-            logging.warn('Google API query returned code\
-                         {}'.format(r.code))
+            logging.warn('Google API query returned code {}'.format(r.code))
 
         # allow images to live for 5 minutes
         t = Timer(600.0, delete_file, [filename])
         t.start()
+
 
 def delete_file(f):
     os.remove(os.path.join(app.config['UPLOAD_DIR'], f))
